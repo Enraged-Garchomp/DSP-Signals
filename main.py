5e80ee286874c891d96c09e9a94fbe612602e7a5
Jason N <jasonjiwoonam@gmail.com>
2026-03-22 18:52:00 -0700

    Changed code to be more concise and clear

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import signals
 import matplotlib.pyplot as plt
 
+
 N = int(input("Enter number of samples to take: "))
-plot_signal = signals.choose_signals(N)
+should_add_signals = input("Would you like to add 2 signals? (yes/no) ")
+if should_add_signals == "yes":
+    signal_1 = input("What type of signal would you like (function 1)? (sine, cosine, linear, impulse) ")
+    signal_2 = input("What type of signal would you like (function 2)? (sine, cosine, linear, impulse) ")
+    f1 = int(input("What is the frequency you would like to add (function 1)? "))
+    f2 = int(input("What is the frequency you would like to add (function 2)? "))
+    signal_to_plot = signals.two_signals(signal_1, signal_2, N, f1, f2)
+elif should_add_signals == "no":
+    signal_type = input("What kind of signal would you like? (sine, cosine, linear, impulse) ")
+    f = int(input("What is the frequency you would like? "))
+    signal_to_plot = signals.one_signal(signal_type, N, f)
+else:
+    raise ValueError("Please enter either 'yes' or 'no'")
+
+plot_signal = signal_to_plot
 
 print(f"Values: {plot_signal}")
 
--- signals.py
+++ signals.py
@@ -1,96 +1,56 @@
 import math
 
 #Signal for y = 1
-def constant_signal(N):
+def constant_signal(N, number):
     """Creates constant signal of 1"""
-    signal = []
-    for _ in range(N):
-        signal.append(1)
+    signal = [number for _ in range(N)]
+
     return signal
 
 #Impulse signal
-def impulse_signal(N):
+def impulse_signal(N, number):
     """Creates Impulse Signal of 1 and 0"""
-    signal = []
-    for n in range(N):
-        if n == 0:
-            signal.append(1)
-        else:
-            signal.append(0)
+    signal = [number if n == 0 else 0 for n in range(N)]
+
     return signal
 
 def sine_signal(N, f):
     """Creates a Sine signal with sampling number N and frequency f"""
-    signal = []
-    for n in range(N):
-        x = math.sin(2 * math.pi * f * n / N)
-        signal.append(x)
+    signal = [math.sin(2 * math.pi * f * n / N) for n in range(N)]
+
     return signal
 
 def cosine_signal(N, f):
     """Creates a Cosine signal with sampling number N and frequency f"""
-    signal = []
-    for n in range(N):
-        x = math.cos(2 * math.pi * f * n / N)
-        signal.append(x)
+    signal = [math.cos(2 * math.pi * f * n / N) for n in range(N)]
+
     return signal
 
 def add_signals(x, y):
     """Adds two signals x and y"""
     if len(x) != len(y):
         raise ValueError("Signals must have same length")
-
-    new_signal = []
-    for n in range(len(x)):
-        new_signal.append(x[n] + y[n])
+    new_signal = [x[n] + y[n] for n in range(len(x))]
 
     return new_signal
 
-def choose_signals(N):
-    """Gives you signals you choose"""
-    should_add_signals = input("Would you like to add 2 signals? (yes/no) ")
-    if should_add_signals == "yes":
-        signal_type_1 = input("What type of signal would you like (function 1)? (sine, cosine, linear, impulse) ")
-        signal_type_2 = input("What type of signal would you like (function 2)? (sine, cosine, linear, impulse) ")
-        if signal_type_1 == "sine":
-            f = int(input("What is the frequency you would like to add (function 1)? "))
-            signal1 = sine_signal(N, f)
-        elif signal_type_1 == "cosine":
-            f = int(input("What is the frequency you would like to add (function 1)? "))
-            signal1 = cosine_signal(N, f)
-        elif signal_type_1 == "linear":
-            signal1 = constant_signal(N)
-        elif signal_type_1 == "impulse":
-            signal1 = impulse_signal(N)
-        else:
-            raise ValueError("Must respond with (sine, cosine, linear, impulse)")
-        if signal_type_2 == "sine":
-            f1 = int(input("What is the frequency you would like to add (function 2)? "))
-            signal2 = sine_signal(N, f1)
-        elif signal_type_2 == "cosine":
-            f1 = int(input("What is the frequency you would like to add (function 2)? "))
-            signal2 = cosine_signal(N, f1)
-        elif signal_type_2 == "linear":
-            signal2 = constant_signal(N)
-        elif signal_type_2 == "impulse":
-            signal2 = impulse_signal(N)
-        else:
-            raise ValueError("Must respond with (sine, cosine, linear, impulse)")
-        final_signal = add_signals(signal1, signal2)
-        return final_signal
-
-    elif should_add_signals == "no":
-        signal_type = input("What type of signal would you like? (sine, cosine, linear, impulse) ")
-        if signal_type == "sine":
-            f = int(input("What frequency would you like? "))
+def one_signal(type, N, f):
+    """Creates one signal with sampling number N"""
+    if type in ["sine", "cosine"]:
+        if type == "sine":
             signal = sine_signal(N, f)
-        elif signal_type == "cosine":
-            f = int(input("What frequency would you like? "))
+        else:
             signal = cosine_signal(N, f)
-        elif signal_type == "linear":
-            signal = constant_signal(N)
-        elif signal_type == "impulse":
-            signal = impulse_signal(N)
     else:
-        raise ValueError("Must respond with yes/no")
+        if type == "linear":
+            signal = constant_signal(N, f)
+        else:
+            signal = impulse_signal(N, f)
     return signal
+
+def two_signals(type1, type2, N, f1, f2):
+    signal1 = one_signal(type1, N, f1)
+    signal2 = one_signal(type2, N, f2)
+    signal = add_signals(signal1, signal2)
+    return signal
+
