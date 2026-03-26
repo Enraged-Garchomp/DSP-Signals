5feeba709eb5407e57998370f70c3bba67ab2b8e
Jason N <jasonjiwoonam@gmail.com>
2026-03-25 22:23:36 -0700

    Added some input validation

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ def get_param(signal_type):
     return param
 
 N = int(input("Enter number of samples to take: "))
-should_add_signals = input("Would you like to add 2 signals? (yes/no) ")
+should_add_signals = input("Would you like to add 2 signals? (yes/no) ").strip().lower()
 if should_add_signals == "yes":
-    signal_1 = input("What type of signal would you like (function 1)? (sine, cosine, constant, impulse) ")
+    signal_1 = input("What type of signal would you like (function 1)? (sine, cosine, constant, impulse) ").strip().lower()
     param1 = get_param(signal_1)
-    signal_2 = input("What type of signal would you like (function 2)? (sine, cosine, constant, impulse) ")
+    signal_2 = input("What type of signal would you like (function 2)? (sine, cosine, constant, impulse) ").strip().lower()
     param2 = get_param(signal_2)
     signal_to_plot = signals.two_signals(signal_1, signal_2, N, param1, param2)
 elif should_add_signals == "no":
-    signal_type = input("What kind of signal would you like? (sine, cosine, constant, impulse) ")
+    signal_type = input("What kind of signal would you like? (sine, cosine, constant, impulse) ").strip().lower()
     param = get_param(signal_type)
     signal_to_plot = signals.one_signal(signal_type, N, param)
 else:
